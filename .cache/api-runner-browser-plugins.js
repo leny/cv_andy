80ec6f49ef8e0718891491bba5572afe12172939
Krause <AndyKrausePro@gmail.com>
2019-02-23 01:38:51 +0100

    andy krause cv final

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/home/krause/Documents/cv_andy/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/home/krause/Documents/cv_andy/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
