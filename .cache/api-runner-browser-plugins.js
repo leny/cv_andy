3ee97a90598b0ad3b2338cc4621217e299de9e09
leny <info@flatland.be>
2019-02-23 13:21:46 +0100

    Remove cache

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/home/krause/Documents/cv_andy/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/home/krause/Documents/cv_andy/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
